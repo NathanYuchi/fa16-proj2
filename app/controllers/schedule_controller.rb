0cd7a6c93a7b116a2106380ba84886e45a7bee34
Nathan Yuchi <nathanyuchi@gmail.com>
2016-11-28 21:09:13 -0800

    wut

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,7 +1,12 @@
 class ScheduleController < ApplicationController
   def view
+
   end
 
   def add
+    @titles = Array.new
+    Course.all.each do |course|
+      @titles.push(course.title)
+    end
   end
 end
