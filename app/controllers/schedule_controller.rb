c0d234760fd1f877f69285682ff7b9ec6d24931c
peitianx <peitian.xiong@berkeley.edu>
2016-11-29 02:18:22 -0800

    update add function

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,15 +1,16 @@
 class ScheduleController < ApplicationController
-  def view
+  	def view
+  		@course = Course.where(:user_id => current_user)
+  	
+ 	end
 
-  end
+ 	def index
+   		@courses = Course.all
+ 	end
 
- def index
-    @courses = Course.all
-  end
-
-def add
-  	@course = Course.where(id: params[:id])
-  	@course.update_all(:user_id => current_user)
-  	#redirect_to "/"
-end
+	def add
+	  	@course = Course.where(id: params[:id])
+	  	@course.update_all(:user_id => current_user)
+	  	redirect_to schedule_view_path(:user_id => current_user)
+  	end
 end
